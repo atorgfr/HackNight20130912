def0537982791b962e883d8f5a308734c8080639
Atorgfr <gregory.rota@gmail.com>
2013-09-12 21:18:57 +0200

    add gmap js

---
+++ js/gmaps.js
@@ -0,0 +1,43 @@
+window.hackmap = window.hackmap || {};
+
+(function(hackmap){
+
+	hackmap.map ={};
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+	      'callback=hackmap.getmap';
+	document.body.appendChild(script);
+
+	hackmap.getmap = function()
+	{
+	  hackmap.map = initialize();
+	};
+
+	hackmap.addMarker= function(lat, long, url)
+	{
+		var image = {
+		    url: url,
+		    size: new google.maps.Size(50, 50)
+		  };
+
+		var myLatlng = new google.maps.LatLng(lat, long);
+	    
+	    var marker = new google.maps.Marker({
+	      position: myLatlng,
+	      map: hackmap.map,
+	      icon: image
+	    });
+	}
+
+	function initialize() {
+	  var mapOptions = {
+	    zoom: 2,
+	    center: new google.maps.LatLng(46.000, 2.000),
+	    mapTypeId: google.maps.MapTypeId.ROADMAP
+	  };
+
+	  return new google.maps.Map(document.getElementById('map-canvas'),
+	      mapOptions);
+	}
+})(hackmap);
